bf497a38c2182087969b4579e40e9b0f4cf533b0
Wanderson Ramos <wandersonramos740@gmail.com>
2023-09-14 22:16:24 -0300

    add project

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent {
+  @Input()
+  gameType: string = '';
+
+  @Input()
+  gamePrice: string = ''
+}
